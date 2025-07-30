c36897144aede240bd503f2af84336f46003e3e2
Gourav-Sharma-23BCS10857 <23bcs10857@cuchd.in>
2025-07-30 21:51:40 +0530

    Created experiment2.sql

--- Experiment 2/experiment2.sql
+++ Experiment 2/experiment2.sql
@@ -1,13 +1,11 @@
 -- Medium Level Problem
 CREATE TABLE Employee (
-    EmpID INT PRIMARY KEY,
-    EmpName VARCHAR(50) NOT NULL,
-    Department VARCHAR(50) NOT NULL,
-    ManagerID INT NULL  
+    EmpID INTEGER PRIMARY KEY,
+    EmpName TEXT NOT NULL,
+    Department TEXT NOT NULL,
+    ManagerID INTEGER,
+    FOREIGN KEY (ManagerID) REFERENCES Employee(EmpID)
 );
-
-ALTER TABLE Employee
-ADD CONSTRAINT FK_Manager FOREIGN KEY (ManagerID) REFERENCES Employee(EmpID);
 
 INSERT INTO Employee (EmpID, EmpName, Department, ManagerID)
 VALUES
@@ -19,13 +17,17 @@ VALUES
 (6, 'Frank', 'HR', 1);
 
 SELECT 
-E.EmpName AS [EmployeeName], 
-E.Department AS [EmployeeDept], 
-M.EmpName AS [Manager Name], 
-M.Department AS [ManagerDept]
-FROM Employee AS E
-JOIN Employee AS M 
-ON E.ManagerId = M.EmpID;
+    E.EmpName AS EmployeeName, 
+    E.Department AS EmployeeDept, 
+    M.EmpName AS ManagerName, 
+    M.Department AS ManagerDept
+FROM 
+    Employee E
+JOIN 
+    Employee M 
+ON 
+    E.ManagerID = M.EmpID;
+
 
 --Hard Level Problem
 
